583f50596b55b219eac658812542121cbe2f4000
Lucas Ferreira <lks_ferreira@hotmail.com>
2022-05-01 10:02:09 -0300

    att

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 let textoEntrada = document.querySelector("textarea");
 let textoSaida = document.querySelector("article");
+const btnCripto = document.getElementById("cripto");
 
-textoSaida.textContent = textoEntrada.value;
+textoEntrada = "A função de script é para exectuar algo dentro do código"
+
+function testeFuncional() {
+    console.log("Teste padrão de executação");
+}
+
+function criptografa() {
+    if (textoEntrada.value != null) {
+        textoSaida.textContent = textoEntrada.value;
+    } else {
+        textoSaida.textContent = textoEntrada;
+        
+    }
+}
+
+
+btnCripto.addEventListener("click", testeFuncional);
+btnCripto.addEventListener("click", criptografa);
