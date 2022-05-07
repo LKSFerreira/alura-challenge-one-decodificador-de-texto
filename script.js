78c25abc6d73f11ce9f27fe0c358b802429d9f58
Lucas Ferreira <lks_ferreira@hotmail.com>
2022-05-06 23:42:02 -0300

    implementado descriptografia e algumas correções.
    Atualizado matriz de criptografia, alterada algumas tag, implementado função de descriptografia, atualziado algumas variaveis, criado microfunções.

--- script.js
+++ script.js
@@ -1,90 +1,118 @@
 const textoArea = document.querySelector(".text_content");
 let textoSaida = document.querySelector(".texto-saida");
 const btnCripto = document.getElementById("cripto");
+const espaco = " ";
 
-const mapaCaracteres = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "ç", "Ç", "á", "Á", "é", "â", "Â", "ê", "Ê", "à", "À", "É", "ã", "Ã", "õ", "Õ", "í", "Í", "ó", "Ó", "ú", "Ú", "ñ", "Ñ", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "-", ",", ".", ";", "!", "?", " ",];
+const mapaCaracteres = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "ç", "Ç", "á", "Á", "é", "â", "Â", "ê", "Ê", "à", "À", "É", "ã", "Ã", "õ", "Õ", "í", "Í", "ó", "Ó", "ú", "Ú", "ñ", "Ñ", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "-", ",", ".", ";", "!", "?", espaco];
 
-const kryto = ["æ", "øұ", "æ≠", "ø∑", "£ʡ", "£ü", "ïñ", "çz", "£", "ƒ", "¿", "¥", "¤", "ð", "ẟß", "µ", "§", "2ú", "ôå", "ö&", "é½", "%", "?", "§", "ʭ", "ʢ", "ͼ", "β", "ѧ", "ѫ", "ѳ", "ѻ", "ұ", "֏", "ᵫ", "ᵽ", "ẟ", "Ỽ", "šf", "žʩ", "æñ", "ʭʨ", "₿", "₡", "₣", "₦", "€", "₸", "₼", "⅍", "∞", "≠", "∑", "ꟽ", "ʡ", "ʣ", "ʥ", "ʦ", "ʧ", "ʨ", "ʩ", "ω", "ϣ", "ж", "я", "ѥ", "♫", "ӿ", "ԅ", "ժ", "է", "ֆ", "շ", "ᴟ", "ᵆ", "ᵶ", "ᵫ", "‰", "‡", "‖", "†", "æ∞", "šf", "žʩ", "æñ", "ʭʨ", "i@", "hã", "£ø", "∞ᕬ", "∽ϛ", "¥ѻ", "₼§", "?¿", "ժ¤", "ӿ♫", "≠ѫ",];
+const kryto = ["æ", "ø", "≠", "ʡ", "ü", "ï", "£", "ƒ", "¿", "¥", "¤", "ð", "ß", "µ", "§", "å", "ö", "&", "½", "%", "?", "ʭ", "ʢ", "ͼ", "β", "ѧ", "ѫ", "ѳ", "ѻ", "ұ", "֏", "ᵫ", "ᵽ", "ẟ", "Ỽ", "š", "ž", "ʩ", "ʨ", "₿", "₡", "₣", "₦", "€", "₸", "⅍", "∑", "ꟽ", "ʣ", "ʥ", "ʦ", "ʧ", "ω", "ϣ", "ж", "я", "ѥ", "♫", "ӿ", "ԅ", "ժ", "է", "ֆ", "շ", "ᴟ", "ᵆ", "ᵶ", "‰", "‡", "‖", "†", "∞", "@", "ᕬ", "∽", "ϛ", "₼", "¢", "©", "¶", "®", "¦", "±", "¯", "·", "º", "Þ", "¬", "»", "¼", "¾", "°", espaco];
 
-let textoEntrada = [];
 let textoCriptografado = [];
+let textoEntrada = [];
 
-
-
-// Transfere o texto do Area de texto para o lado em que deverá ser descriptogrado
+// Transfere o texto do Area de texto para o lado em que deverá ser criptogrado
 function transfere() {
-
-    textoEntrada = textoArea.value;
+    verificaValorEntrada()
 
     criptografa();
 
-    if (textoEntrada == "") {
-        alert("Por gentileza digite algo para criptografar");
-    }
-
+    /*    if (textoEntrada == "" || textoEntrada == " ") {
+            textoEmBranco();
+        }
+    */
     mostraSaida(textoCriptografado);
     textoCriptografado = [];
 }
+// Final da transferencia
 
-function testeFuncional() {
-    console.log("TESTE EXECUTADO COM SUCESSO");
-}
+btnCripto.addEventListener("click", transfere);
 
-btnCripto.addEventListener("click", transfere); // Final da transferencia
-
-
+// Cripotagra o texto contido na Area de Texto de Entrada - Utilizando um modelo de criptografia conhecido como substituição
 function criptografa() {
+    verificaValorEntrada()
 
     for (let i = 0; i < textoEntrada.length; i++) {
-
-        letra = textoEntrada[i];
+        textoEntrada[i];
+        // console.log(letra);
 
         for (let j = 0; j < mapaCaracteres.length; j++) {
-            if (letra == mapaCaracteres[j] && letra.length > 0) {
-                testeFuncional();
-                console.log(letra, mapaCaracteres[j])
+            if (textoEntrada[i] == mapaCaracteres[j] && textoEntrada[i].length > 0) {
+
+                //console.log(textoEntrada[i], mapaCaracteres[j])
                 letras = textoEntrada[i].replace(mapaCaracteres[j], kryto[j])
                 textoCriptografado = textoCriptografado + letras;
-                console.log(textoCriptografado);
-
-            } else if (letra == "") {
-                alert("Por gentileza digite algo para criptografar");
-                break;
+                //console.log(textoCriptografado);
             }
         }
     } return
+}//Fim da Criptografia
+
+const btbDescripto_02 = document.querySelector("#descriptografa_02")
+const btnDescripto = document.querySelector("#descriptografa_01");
+btnDescripto.addEventListener("click", descripta);
+btbDescripto_02.addEventListener("click", descripta);
+
+// Tranfere o texto criptografado para a Texto de Saida
+function descripta() {
+    verificaValorEntrada()
+    descriptografa();
+
+    mostraSaida(textoCriptografado);
+    textoCriptografado = [];
+} // Fim da função
+
+// Descriptografa o Texto de Entrada pelo método de substituição
+function descriptografa() {
+    verificaValorEntrada()
+
+    for (let i = 0; i < textoEntrada.length; i++) {
+        textoEntrada[i];
+
+        for (let j = 0; j < kryto.length; j++) {
+            if (textoEntrada[i] == kryto[j] && textoEntrada[i].length > 0) {
+
+                //console.log(textoEntrada[i], mapaCaracteres[j])
+                letras = textoEntrada[i].replace(kryto[j], mapaCaracteres[j])
+                textoCriptografado = textoCriptografado + letras;
+                console.log(textoCriptografado);
+            }
+
+        }
+    } return
+} // Fim da função
+
+const copia = document.querySelector("#copia");
+copia.addEventListener("click", copiaTexto);
+
+//Realiza a cópia do conteúdo criptografado, valor contido no Texto de Saida
+function copiaTexto() {
+
+    if (textoEntrada == "" || textoSaida.textContent == " ") {
+        textoEmBranco();
+    } else {
+        navigator.clipboard.writeText(textoSaida.textContent);
+        textoSaida.textContent = "Copiado com sucesso!";
+        textoArea.value = "";
+    }
+}//Fim da função que copia
+
+const cola = document.querySelector("#cola");
+cola.addEventListener("click", colaTexto);
+
+//Cola o conteúdo da area de transferência para a Área de Texto de Entrada
+function colaTexto() {
+    navigator.clipboard.readText().then(
+        clipTexto => textoArea.value = clipTexto);
+}//fim da função colar
+
+function textoEmBranco() {
+    alert("É necessário criptografar algo antes!");
 }
 
 function mostraSaida(texto) {
     textoSaida.textContent = texto;
 }
 
-// Função copiar texto de saida para are de transferencia:
-
-const copia = document.querySelector("#copia");
-
-copia.addEventListener("click", copiaTexto);
-
-function copiaTexto() {
-
+function verificaValorEntrada() {
     textoEntrada = textoArea.value;
-
-    if (textoEntrada == "") {
-        textoEmBranco();
-
-    } else if (textoSaida.textContent == " ") {
-        textoEmBranco();
-    } else {
-        navigator.clipboard.writeText(textoSaida.textContent)
-        textoSaida.textContent = "Copiado com sucesso!";
-        textoEntrada = "";
-
-        console.log(textoSaida);
-    }
 }
-
-function textoEmBranco() {
-    alert("É necessário criptografar algo antes!");
-}
-
-
